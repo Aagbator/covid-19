b84a65d21c0f1b9419881d3653b94c1930b25358
Anthony Agbator <tonjoe99@gmail.com>
2020-04-09 00:40:21 +0100

    updated challenge 1 codebase

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,17 @@
 const calculateImpact = (data) => ({
   // currentlyInfected: data.reportedCases * 10,
   // infectionsByRequestedTime: data.currentlyInfected * 1024
-})
+});
 
 const calculateSevereImpact = (data) => ({
   // currentlyInfected: data.reportedCases * 50,
   // infectionsByRequestedTime: data.currentlyInfected * 1024
-})
+});
 
 const covid19ImpactEstimator = (data) => ({
   data,
   impact: calculateImpact(data),
   severeImpact: calculateSevereImpact(data)
-})
+});
 
 export default covid19ImpactEstimator;
