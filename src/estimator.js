8031843ec420c286177fc9deae57d8960bf81db1
Anthony Agbator <tonjoe99@gmail.com>
2020-04-09 00:13:08 +0100

    challenge 1 completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
+
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
+
+  return {
+    data, // the input data you got
+    impact, // your best case estimation
+    severeImpact // your severe case estimation
+  }
+};
 
 export default covid19ImpactEstimator;
