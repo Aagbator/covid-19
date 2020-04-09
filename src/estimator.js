5510557bb5db3ab247b2f8ddaf80ff07de7ea40e
Anthony Agbator <tonjoe99@gmail.com>
2020-04-09 12:27:06 +0100

    retest challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -12,47 +12,26 @@ const convertToDays = (periodType, timeToElapse) => {
 };
 
 const calculateImpact = (data) => {
-  const {
-    periodType, timeToElapse, reportedCases, totalHospitalBeds
-  } = data;
+  const { periodType, timeToElapse } = data;
 
   const timeInDays = convertToDays(periodType, timeToElapse);
-  const currentlyInfected = reportedCases * 10;
+  const currentlyInfected = data.reportedCases * 10;
   const infectionsByRequestedTime = currentlyInfected * (2 ** (Math.round(timeInDays / 3)));
-  const severeCasesByRequestedTime = (0.15 * infectionsByRequestedTime);
-  const hospitalBedsByRequestedTime = () => {
-    const availableBeds = totalHospitalBeds * 0.35;
-    return availableBeds - severeCasesByRequestedTime;
-  };
 
   return {
     currentlyInfected,
-    infectionsByRequestedTime,
-    severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime
+    infectionsByRequestedTime
   };
 };
 
 const calculateSevereImpact = (data) => {
-  const {
-    periodType, timeToElapse, reportedCases, totalHospitalBeds
-  } = data;
-
-  const timeInDays = convertToDays(periodType, timeToElapse);
-  const currentlyInfected = reportedCases * 50;
+  const timeInDays = convertToDays(data.periodType, data.timeToElapse);
+  const currentlyInfected = data.reportedCases * 50;
   const infectionsByRequestedTime = currentlyInfected * (2 ** Math.round(timeInDays / 3));
-  const severeCasesByRequestedTime = (0.15 * infectionsByRequestedTime);
-
-  const hospitalBedsByRequestedTime = () => {
-    const availableBeds = totalHospitalBeds * 0.35;
-    return availableBeds - severeCasesByRequestedTime;
-  };
 
   return {
     currentlyInfected,
-    infectionsByRequestedTime,
-    severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime
+    infectionsByRequestedTime
   };
 };
 
