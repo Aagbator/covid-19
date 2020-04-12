2a17e6f14cb2ee09d8624d5891773d73c3645198
Anthony Agbator <tonjoe99@gmail.com>
2020-04-12 10:04:29 +0100

    completed

--- src/estimator.js
+++ src/estimator.js
@@ -66,18 +66,3 @@ const covid19ImpactEstimator = (data) => ({
 });
 
 export default covid19ImpactEstimator;
-
-
-// {
-//   region: {
-//     name: "Africa",
-//       avgAge: 19.7,
-//       avgDailyIncomeInUSD: 5,
-//       avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: "days",
-//     timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// }
